b845130255e8aa22e78d569688f6c62379cd39ba
Gregor <gregorlthomson@sky.com>
2021-11-15 18:08:53 +0000

    Get user choice

--- script.js
+++ script.js
@@ -9,8 +9,25 @@ const outcomeText = document.querySelector(".outcome-text");
 
 // When the user chooses a move:
 
+// Assign the user's choice to a variable
+
+const getUserChoice = (event) => {
+  const userChoice = event.target.innerText;
+  return "User chose: " + userChoice;
+};
+
 // Generate a move for the computer
 
 // Work out the winner
 
 // Display the winner
+
+// Write a function to add an eventlistener to an element
+
+function updateDOM(event) {
+  outcomeText.textContent = getUserChoice(event);
+}
+
+userButtons.forEach((element) => {
+  element.addEventListener("click", updateDOM);
+});
