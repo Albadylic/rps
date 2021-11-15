09ba02163c7c2bf1b5212bdc2c209e8dea278fd8
Gregor <gregorlthomson@sky.com>
2021-11-15 17:31:11 +0000

    Add broad steps to solve the problem

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello");
+// Grab the buttons from the page
+
+// Get the computer-choice and outcome elements from HTML
+
+// When the user chooses a move:
+
+// Generate a move for the computer
+
+// Work out the winner
+
+// Display the winner
