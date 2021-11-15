07a1e2a5409539aa1e6409ea237985f10d219b6f
Gregor <gregorlthomson@sky.com>
2021-11-15 17:43:30 +0000

    Get elements from the page

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Grab the buttons from the page
 
+const userButtons = document.querySelectorAll(".move-button");
+
 // Get the computer-choice and outcome elements from HTML
+
+const computerChoice = document.querySelector("#computer-choice");
+const outcomeText = document.querySelector(".outcome-text");
 
 // When the user chooses a move:
 
